4a5726588f9d4bcc9a0515dd5cb798d706d5dfc9
mansourlotfi <mansourlotfi@gmail.com>
2020-01-12 14:45:16 +0330

    add icon and elements to buttontabbar

--- android/app/src/navigation/ItemsNavigator.js
+++ android/app/src/navigation/ItemsNavigator.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
+import { Button, Icon, ThemeProvider } from 'react-native-elements';
 import CategoryScreen from '../screens/CategoryScreen';
 import CategoryItemScreen from '../screens/categoryItemsScreen';
 import ItemDetailScreen from '../screens/ItemDetailScreen';
@@ -45,20 +46,20 @@ const ItemsNavigator = createStackNavigator(
 const TabNavigator = createBottomTabNavigator(
 	{
 		خانه: {
-			screen: ItemsNavigator
-			// navigationOptions: {
-			// 	tabBarIcon: (tabInfo) => {
-			// 		return <Ionicons name="ios-restaurant" size={25} color={tabInfo.tintColor} />;
-			// 	}
-			// }
+			screen: ItemsNavigator,
+			navigationOptions: {
+				tabBarIcon: (tabInfo) => {
+					return <Icon reverse name="ios-home" type="ionicon" size={25} color={tabInfo.tintColor} />;
+				}
+			}
 		},
 		ثبت: {
-			screen: UserInputScreen
-			// navigationOptions: {
-			// 	tabBarIcon: (tabInfo) => {
-			// 		return <Ionicons name="ios-save" size={25} color={tabInfo.tintColor} />;
-			// 	}
-			// }
+			screen: UserInputScreen,
+			navigationOptions: {
+				tabBarIcon: (tabInfo) => {
+					return <Icon reverse name="add-circle" size={25} color={tabInfo.tintColor} />;
+				}
+			}
 		}
 	},
 	{
